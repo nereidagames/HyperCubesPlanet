fd1b303dd672546afca3431b07cc34355019290e
nereidagames <mkaluza09@gmail.com>
2025-11-27 17:26:26 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+export const API_BASE_URL = 'https://hypercubes-nexus-server.onrender.com';
+
+export const STORAGE_KEYS = {
+    PLAYER_NAME: 'bsp_clone_player_name',
+    JWT_TOKEN: 'bsp_clone_jwt_token',
+    USER_ID: 'bsp_clone_user_id',
+    FPS_ENABLED: 'bsp_clone_fps_enabled'
+};
+
+export const LOADING_TEXTS = [
+    "Dziurkowanie Kawałków Sera...",
+    "Wprowadzanie Przycisku Skoku...",
+    "Naprawianie Błędów...",
+    "Ustawianie Grawitacji...",
+    "Generowanie Sześciennych Światów...",
+    "Karmienie Chomików w Serwerowni...",
+    "Wczytywanie - Proszę czekać..."
+];
