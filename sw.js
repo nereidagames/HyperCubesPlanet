d4ee61832f2332a43dd2934194e0057c4c246067
nereidagames <mkaluza09@gmail.com>
2025-12-25 15:26:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'hypercubesplanet-dev-v5'; // ZMIANA WERSJI NA V5
+/* PLIK: sw.js */
+const CACHE_NAME = 'hypercubesplanet-dev-v6'; // Podbita wersja cache
 
 // Lista plików, które mają być dostępne offline.
 const urlsToCache = [
@@ -15,21 +16,35 @@ const urlsToCache = [
   './multiplayer.js',
   './CoinManager.js',
   './BlockManager.js',
+  './IntroManager.js', // Dodano IntroManager
+  './StarterSkins.js', // Dodano StarterSkins
   
-  // Menedżery budowania
+  // Menedżery
   './BuildManager.js',
   './SkinBuilderManager.js',
   './PrefabBuilderManager.js',
   './HyperCubePartBuilderManager.js',
   './BuildCameraController.js',
+  './FriendsManager.js',
+  './MailManager.js',
+  './NewsManager.js',
+  './HighScoresManager.js',
+  './WallManager.js',
 
-  // Skrypty przechowujące dane (Storage)
+  // Storage
   './WorldStorage.js',
   './SkinStorage.js',
   './PrefabStorage.js',
   './HyperCubePartStorage.js',
 
-  // Ikony UI i aplikacji
+  // Config & Templates
+  './Config.js',
+  './UITemplates.js',
+  './GameCore.js',
+  './GameStateManager.js',
+  './AssetLoader.js',
+
+  // Ikony UI
   'icons/favicon.png',
   'icons/icon-play.png',
   'icons/icon-build.png',
@@ -40,6 +55,14 @@ const urlsToCache = [
   'icons/icon-friends.png',
   'icons/icon-jump.png',
   'icons/icon-back.png',
+  'icons/icon-like.png',
+  'icons/icon-chat.png',
+  'icons/icon-share.png',
+  'icons/icon-level.png',
+  'icons/icon-check.png',
+  'icons/icon-home.png',
+  'icons/icon-restart.png',
+  'icons/icon-next.png',
   'icons/logo-poczta.png',
   'icons/icon-newhypercube.png',
   'icons/icon-newhypercubepart.png',
@@ -49,22 +72,55 @@ const urlsToCache = [
   'icons/icon-mediumworld.png',
   'icons/icon-bigworld.png',
   'icons/icon-download.png',
+  'icons/icon-bar.png',
+  'icons/alert.png',
+  'icons/usmiech.png',
+  'icons/gamepad.png',
+  'icons/NavigationButton.png',
+  'icons/sciana.png',
+  'icons/arrow-left.png',
+  'icons/arrow-right.png',
+  'icons/vip.png',
+  'icons/vip_badge.png',
+  'icons/szukaj.png',
+  'icons/wtymswiecie.png',
+  'icons/grazinnymi.png',
+  'icons/misje.png',
+  'icons/nagrody.png',
+  'icons/highscores.png',
+  'icons/tworzenie.png',
+  'icons/bezpieczenstwo.png',
+  'icons/opcje.png',
+  'icons/wyloguj.png',
 
-  // Tekstury bloków
+  // Tekstury bloków (STARE)
   'textures/ziemia.png',
   'textures/trawa.png',
   'textures/drewno.png',
   'textures/piasek.png',
-  'textures/beton.png'
+  'textures/beton.png',
+
+  // Tekstury bloków (NOWE)
+  'textures/dzins.png',
+  'textures/karton.png',
+  'textures/cegla.png',
+  'textures/drewnianapodloga.png',
+  'textures/metalowasiatka.png',
+  'textures/bruk.png',
+  'textures/kamien.png',
+  'textures/otoczak.png',
+  'textures/metalowaplyta.png',
+  'textures/granit.png',
+  'textures/cukierek.png',
+  'textures/gladki.png'
 ];
 
 // Instalacja Service Workera
 self.addEventListener('install', event => {
-  console.log('Service Worker v4: Instalacja...');
+  console.log('Service Worker v6: Instalacja...');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Service Worker v4: Cache otwarty');
         return cache.addAll(urlsToCache);
       })
       .then(() => self.skipWaiting())
@@ -73,7 +129,7 @@ self.addEventListener('install', event => {
 
 // Aktywacja i czyszczenie starych cache
 self.addEventListener('activate', event => {
-  console.log('Service Worker v4: Aktywacja...');
+  console.log('Service Worker v6: Aktywacja...');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
